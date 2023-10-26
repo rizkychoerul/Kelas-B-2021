c96a18b4487cbea26b033d8dee036260d751e5f2
Rizky C <146051421+rizkychoerul@users.noreply.github.com>
2023-10-26 11:05:01 +0700

    Delete example-app directory

--- example-app/resources/views/contoh.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('title', 'Halaman contoh')
-    @section('content')
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni aspernatur, necessitatibus eum quis aut illo fugiat debitis dolorum enim ab officia molestiae autem pariatur accusantium nisi nulla illum placeat fuga?
-    @endsection
-@endsection
