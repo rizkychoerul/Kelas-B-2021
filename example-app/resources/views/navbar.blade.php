c18cb23f525ea1a2f23df6c0455cb76b2b220568
rizkychoerul <146051421+rizkychoerul@users.noreply.github.com>
2023-10-26 03:47:52 +0000

    Pending changes exported from your codespace

---
+++ example-app/resources/views/navbar.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <a href="">Brand</a>
+        <div>
+            <a href="">Landing</a>
+            <a href="">Account</a>
+            <a href="">Work</a>
+            <a href="">Blog</a>
+        </div>
+    </nav>
+</header>
